148d50c956f845a046426c38dd4a1c8f0ff96eb9
Kin Zheng <kinwzz93@gmail.com>
2023-06-14 17:41:58 -0600

    added snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,13 @@ import streamlit
 import pandas as pd
 import requests
 import snowflake.connector
+#Snowflake connection details
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snoflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(),CURRENT_ACCOUNT(),CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
 streamlit.title("My Mom's New Healthy Diner")
 
